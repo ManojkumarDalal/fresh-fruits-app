d707aa5dae0798c9c46f404432a3c77e9bf7dd22
Manojkumar Dalal <rajhansmanoj77@gmail.com>
2024-08-11 15:43:19 +0530

    implementing scroll functionality

--- src/components/Banners/Banner.jsx
+++ src/components/Banners/Banner.jsx
@@ -2,72 +2,76 @@ import React from "react";
 import BannerImg from "../../assets/fruits-splash.png";
 import { motion } from "framer-motion";
 import { FadeLeft, FadeUp } from "../../utility/Animation";
+import { Element } from "react-scroll";
 
 const Banner = () => {
   return (
-    <section className="">
-      <div className="container bg-secondary/10 grid grid-cols-1 md:grid-cols-2 space-y-6 md:space-y-0 py-14">
-        {/* Banner Image */}
-        <div className="flex justify-center items-center">
-          <motion.img
-            initial={{ opacity: 0, scale: 0.5 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            transition={{ type: "Spring", stiffness: 100, delay: 0.5 }}
-            viewport={{ once: true }}
-            src={BannerImg}
-            alt=""
-            className="w-[300px] md:max-w-[400px] h-full object-cover"
-          />
-        </div>
-        {/* Brand Info */}
-        <div className="flex flex-col justify-center">
-          <div className="text-center md:text-left space-y-4 lg:max-w-[400px]">
-            <motion.h1
-              variants={FadeUp(0.5)}
-              initial="hidden"
-              whileInView="visible"
+    <Element name="about">
+      <section id="about">
+        <div className="container bg-secondary/10 grid grid-cols-1 md:grid-cols-2 space-y-6 md:space-y-0 py-14">
+          {/* Banner Image */}
+          <div className="flex justify-center items-center">
+            <motion.img
+              initial={{ opacity: 0, scale: 0.5 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              transition={{ type: "Spring", stiffness: 100, delay: 0.5 }}
               viewport={{ once: true }}
-              className="text-3xl text-center lg:text-6xl font-bold uppercase w-[450px] mb-[30px]"
-            >
-              brand info
-            </motion.h1>
-            <motion.p
-              variants={FadeUp(0.7)}
-              initial="hidden"
-              whileInView="visible"
-              viewport={{ once: true }}
-              className="text-xl text-justify w-[550px]"
-            >
-              "Healthy Fruit Sales offers the freshest, highest-quality fruits,
-              sourced directly from sustainable farms. We prioritize health and
-              flavor, ensuring every bite is packed with nutrients and taste.
-            </motion.p>
-            <motion.p
-              variants={FadeUp(0.9)}
-              initial="hidden"
-              whileInView="visible"
-              viewport={{ once: true }}
-              className="text-xl text-justify w-[550px]"
-            >
-              Discover a wide range of delicious, hand-picked fruits perfect for
-              your daily needs. Freshness guaranteed, from our farms to your
-              table."
-            </motion.p>
-            {/* Button Section */}
-            <motion.div
-              variants={FadeUp(1.1)}
-              initial="hidden"
-              animate="visible"
-              className="flex justify-center md:justify-start"
-            >
-              <button className="text-xl primary-btn hover:scale-110 p-3 mt-[30px]">
-                Learn More
-              </button>
-            </motion.div>
+              src={BannerImg}
+              alt=""
+              className="w-[300px] md:max-w-[400px] h-full object-cover"
+            />
+          </div>
+          {/* Brand Info */}
+          <div className="flex flex-col justify-center">
+            <div className="text-center md:text-left space-y-4 lg:max-w-[400px]">
+              <motion.h1
+                variants={FadeUp(0.5)}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                className="text-3xl text-center lg:text-6xl font-bold uppercase w-[450px] mb-[30px]"
+              >
+                brand info
+              </motion.h1>
+              <motion.p
+                variants={FadeUp(0.7)}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                className="text-xl text-justify w-[550px]"
+              >
+                "Healthy Fruit Sales offers the freshest, highest-quality
+                fruits, sourced directly from sustainable farms. We prioritize
+                health and flavor, ensuring every bite is packed with nutrients
+                and taste.
+              </motion.p>
+              <motion.p
+                variants={FadeUp(0.9)}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                className="text-xl text-justify w-[550px]"
+              >
+                Discover a wide range of delicious, hand-picked fruits perfect
+                for your daily needs. Freshness guaranteed, from our farms to
+                your table."
+              </motion.p>
+              {/* Button Section */}
+              <motion.div
+                variants={FadeUp(1.1)}
+                initial="hidden"
+                animate="visible"
+                className="flex justify-center md:justify-start"
+              >
+                <button className="text-xl primary-btn hover:scale-110 p-3 mt-[30px]">
+                  Learn More
+                </button>
+              </motion.div>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Element>
   );
 };
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,52 +1,77 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { FaLeaf } from "react-icons/fa6";
-import { MdOutlineShoppingCart } from "react-icons/md";
-import { MdMenu } from "react-icons/md";
+import { MdOutlineShoppingCart, MdMenu } from "react-icons/md";
 import ResponsiveMenu from "./ResponsiveMenu";
 import { motion } from "framer-motion";
+import Banner from "../Banners/Banner";
+import Banner2 from "../Banners/Banner2";
+import Banner3 from "../Banners/Banner3";
+import Hero from "../Hero/Hero";
+import Menus from "../Menus/Menus";
+import Footer from "../Footer/Footer";
 
 const NavbarMenu = [
   {
     id: 1,
     title: "Home",
-    link: "/",
   },
-
   {
     id: 2,
     title: "About",
-    link: "/about",
   },
-
   {
     id: 3,
     title: "Products",
-    link: "/products",
   },
-
   {
     id: 4,
     title: "Shop",
-    link: "/shop",
   },
-
   {
     id: 5,
     title: "Contact",
-    link: "/contact",
   },
 ];
+
 const Navbar = () => {
   const [open, setOpen] = useState(false);
 
+  const Home = useRef(null);
+  const About = useRef(null);
+  const Products = useRef(null);
+  const Shop = useRef(null);
+  const Contact = useRef(null);
+
+  const scrollToSection = (section) => {
+    switch (section) {
+      case "Home":
+        Home.current.scrollIntoView({ behavior: "smooth" });
+        break;
+      case "About":
+        About.current.scrollIntoView({ behavior: "smooth" });
+        break;
+      case "Products":
+        Products.current.scrollIntoView({ behavior: "smooth" });
+        break;
+      case "Shop":
+        Shop.current.scrollIntoView({ behavior: "smooth" });
+        break;
+      case "Contact":
+        Contact.current.scrollIntoView({ behavior: "smooth" });
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <>
-      <nav>
+      <nav className="container">
         <motion.div
-          initial={{opacity: 0}}
-          animate={{opacity: 1}}
-          transition={{duration: 0.5, delay: 0.5}}
-          className="container flex items-center justify-between py-4 md:pt-4"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          className="container fixed left-0 right-0 top-0 z-50 bg-white shadow-md flex items-center justify-between py-4 md:pt-4"
         >
           {/* Logo Section */}
           <div className="text-2xl flex items-center gap-2 font-bold uppercase">
@@ -59,13 +84,14 @@ const Navbar = () => {
           <div className="hidden md:block">
             <ul className="flex items-center gap-4 text-gray-600">
               {NavbarMenu.map((menu) => (
-                <li key={menu.id} className="text-xl font-semibold">
-                  <a
-                    href={menu.link}
-                    className="inline-block py-1 px-3 hover:text-primary hover:shadow-[0_3px_0_-1px_#ef4444]"
-                  >
+                <li
+                  key={menu.id}
+                  onClick={() => scrollToSection(menu.title)}
+                  className="text-xl font-semibold cursor-pointer"
+                >
+                  <span className="inline-block py-1 px-3 hover:text-primary hover:shadow-[0_3px_0_-1px_#ef4444]">
                     {menu.title}
-                  </a>
+                  </span>
                 </li>
               ))}
               <button className="text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200">
@@ -76,12 +102,29 @@ const Navbar = () => {
 
           {/* Mobile Hamburger Menu Section */}
           <div className="md:hidden" onClick={() => setOpen(!open)}>
-            <MdMenu className="text-4xl" />
+            <MdMenu className="text-4xl cursor-pointer" />
           </div>
         </motion.div>
       </nav>
       {/* Mobile Menu Section */}
       <ResponsiveMenu open={open} />
+      
+      {/* Sections */}
+      <div ref={Home} className="mt-[100px]">
+        <Hero />
+      </div>
+      <div ref={About}>
+        <Banner />
+      </div>
+      <div ref={Products}>
+        <Banner2 />
+      </div>
+      <div ref={Shop}>
+        <Banner3 />
+      </div>
+      <div ref={Contact}>
+        <Footer />
+      </div>
     </>
   );
 };
